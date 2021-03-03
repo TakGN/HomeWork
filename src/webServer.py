dc30fc124b99b74c894ae22f3fe545a00888decc
Takwa <takwa.gnenna@gmail.com>
2021-03-03 23:50:48 +0100

    retouching the POST predict endpoint to return a prediction per given model

--- src/webServer.py
+++ src/webServer.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from model import Model
 from persistence import TrainModel
+from parameters import model_path, pipeline_name
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,6 +14,21 @@ class Prediction(Resource):
 
     @staticmethod
     def post():
+        email = request.json.get('email', '')
+        model_name = request.json.get('model_name', pipeline_name)
+        query_df = pd.DataFrame([{'email': email}])
+        model = Model.load_pipeline(model_path, model_name)
+        # Number_requests_processed.inc()
+        return jsonify({'prediction for {}'.format(email): model.predict(query_df).tolist(),
+                        'model_name': model_name
+                        })
+
+
+
+
+class Training(Resource):
+
+    def post(self):
         body = request.json
         query_df = pd.DataFrame(body)
         model = Model.get_model()
